56728c210ccf005bc200795d6a2a7ca66120a001
Pattu S Sahith <sahithpattu@gmail.com>
2025-12-10 09:58:21 +0530

    Time: 0 ms (100.00%) | Memory: 48 MB (24.70%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0) return 1;
+
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1;
+        while (N > 0) {
+            if ((N & 1) == 1) result *= x;
+            x *= x;
+            N >>= 1;
+        }
+        return result;
+        
+    }
+}
