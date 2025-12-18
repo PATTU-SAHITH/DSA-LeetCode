97e8e37c51f17971878f244d9d7b0a12e661a76d
Pattu S Sahith <sahithpattu@gmail.com>
2025-12-18 21:16:21 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (11.04%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        
+        for(int i=digits.length-1; i>=0;i--){
+            int sum = digits[i]+= carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+        
+        if(carry!=0){
+            int[] arr = new int[digits.length+1];
+            arr[0]=carry;
+            int k=1;
+            while(k<arr.length){
+                arr[k]=digits[k-1];
+                k++;
+            }
+            return arr;
+        }
+
+        return digits;
+    }
+}
